e4d00d70fb59298744210af448932125e9d7dce4
Alona <alenavyrodova@gmail.com>
2023-11-27 14:39:33 +0200

    security basic

---
+++ src/main/java/com/example/bookstore/lib/FieldMatchValidator.java
@@ -0,0 +1,28 @@
+package com.example.bookstore.lib;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.util.Objects;
+import org.springframework.beans.BeanWrapperImpl;
+
+public class FieldMatchValidator implements ConstraintValidator<FieldMatch, Object> {
+
+    private String firstFieldName;
+    private String secondFieldName;
+
+    @Override
+    public void initialize(FieldMatch constraintAnnotation) {
+        firstFieldName = constraintAnnotation.first();
+        secondFieldName = constraintAnnotation.second();
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        Object fieldValue = new BeanWrapperImpl(value)
+                .getPropertyValue(firstFieldName);
+        Object fieldMatchValue = new BeanWrapperImpl(value)
+                .getPropertyValue(secondFieldName);
+        return Objects.equals(fieldValue, fieldMatchValue);
+    }
+}
+
---
+++ src/main/java/com/example/bookstore/service/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.bookstore.service;
+
+import com.example.bookstore.dto.UserRegistrationRequestDto;
+import com.example.bookstore.dto.UserResponseDto;
+import com.example.bookstore.exception.RegistrationException;
+import com.example.bookstore.mapper.UserMapper;
+import com.example.bookstore.model.User;
+import com.example.bookstore.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserResponseDto register(UserRegistrationRequestDto requestDto)
+            throws RegistrationException {
+        if (userRepository.findByEmail(requestDto.getEmail()).isPresent()) {
+            throw new RegistrationException("A user with such an email already exists.");
+        }
+        User user = userMapper.toModel(requestDto);
+
+        User saved = userRepository.save(user);
+        UserResponseDto userResponseDto = userMapper.toUserResponseDto(saved);
+        return userResponseDto;
+    }
+}

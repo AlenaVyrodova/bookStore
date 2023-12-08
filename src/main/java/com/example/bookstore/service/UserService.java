edb2b7f6a01281855e460bc11021899728bdb021
Alona <alenavyrodova@gmail.com>
2023-12-08 10:21:04 +0200

    Jwt token was add

--- src/main/java/com/example/bookstore/service/UserService.java
+++ src/main/java/com/example/bookstore/service/UserService.java
@@ -2,7 +2,9 @@ package com.example.bookstore.service;
 
 import com.example.bookstore.dto.user.UserRegistrationRequestDto;
 import com.example.bookstore.dto.user.UserResponseDto;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface UserService {
     UserResponseDto register(UserRegistrationRequestDto requestDto);
 }
